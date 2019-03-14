ede272523d11baa157db3ba8333204da58a4ac86
Samir Jasarat <samirjasarat@gmail.com>
2019-03-14 17:17:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 function happyHolidays(){return "Happy holidays!"}
 
-function happyHolidays(name){return "Happy holidays, ${name}"}
-
-function happyHolidays(name, holiday){return "happy ${holiday}, ${name}"}
